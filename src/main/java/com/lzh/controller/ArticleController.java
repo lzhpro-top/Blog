d33a6d560a157d9611b802b85d547a28e0fae976
lzhon <lzhon@DESKTOP-L0HM471>
2019-04-26 10:22:42 +0800

    速度

---
+++ src/main/java/com/lzh/controller/ArticleController.java
@@ -0,0 +1,14 @@
+package com.lzh.controller;
+/**
+ * 文章控制器类
+ * @ClassName: ArticleController
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @author 梁中华
+ * @date 2019年4月26日 上午10:16:47
+ * @version V1.0 
+ * @Company 山西特信环宇信息技术有限公司
+ */
+
+public class ArticleController {
+
+}
---
+++ src/main/java/com/lzh/controller/manage/ArticleManageController.java
@@ -0,0 +1,13 @@
+package com.lzh.controller.manage;
+/**
+ * 
+ * @ClassName: ArticleManageController
+ * @Description: TODO 文章管理器类
+ * @author 梁中华
+ * @date 2019年4月26日 上午10:17:55
+ * @version V1.0 
+ * @Company 山西特信环宇信息技术有限公司
+ */
+public class ArticleManageController {
+
+}
